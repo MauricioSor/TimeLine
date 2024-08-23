dd36cf5df186fcf92eabb073ea514059f9d2bc64
Mauricio Soria <mauricioutn2017@gmail.com>
2024-08-23 12:06:20 -0300

    Modificacion de scroll

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 const mensajes = [
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-},
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-},
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}, 
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}, 
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}, 
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}, 
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}, 
-{
-    name: "Hola",
-    description: "Llenar este contenido con el hito"
-}
+    {
+        name: "Hola1",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola2",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola3",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola4",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola5",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola6",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola7",
+        description: "Llenar este contenido con el hito"
+    },
+    , {
+        name: "Hola8",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola9",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola10",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola10",
+        description: "Llenar este contenido con el hito"
+    },
+    {
+        name: "Hola10",
+        description: "Llenar este contenido con el hito"
+    }
 ]
+
 
 var html = "";
 mensajes.forEach(e => {
@@ -41,15 +58,15 @@ timeline.innerHTML = html
 
 var _items = document.querySelectorAll(".child")
 _items.forEach(element => {
-    if (element.offsetTop < 300)
+    if (element.offsetTop < 200)
         element.classList.add('_show')
 })
 
 window.addEventListener('scroll', e => {
-    var scroll = document.documentElement.scrollTop
+    var scroll = document.documentElement.scrollTop-100
     var items = document.querySelectorAll(".child")
     items.forEach(elem => {
-        if (elem.offsetTop - window.innerHeight/2 < scroll) {
+        if (elem.offsetTop - window.innerHeight / 2 < scroll) {
             elem.classList.remove('_hide')
             elem.classList.add('_show')
         } else {
